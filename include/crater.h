d22a8ebba3b812423bacb5a6dc5cbaa8a0fc69e9
Lior Rubanenko <liorr@cabeus.local>
2019-05-21 13:59:54 -0700

    created crater.c,h files. Added option for a spherical crater

---
+++ include/crater.h
@@ -0,0 +1,4 @@
+// Form a new crater:
+void createCrater(int gridSize, double *craterDiameter, double xPosition, double yPosition, double **xmat, double **ymat, double **zmat, int craterProfile);
+
+// Place crater ejecta:
